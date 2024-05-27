c03c023284e0202b8cca60ff54dc54e314f73555
Chillihache <thomas.leclerc.istria@gmail.com>
2024-05-27 13:18:31 +0200

    No data

--- models/round.py
+++
@@ -1,9 +0,0 @@
-class Round:
-
-    def __init__(self, name, list_of_matchs, start_date, end_date):
-
-        self.name = name
-        self.list_of_matchs = list_of_matchs
-        self.start_date = start_date
-        self.end_date = end_date
-
